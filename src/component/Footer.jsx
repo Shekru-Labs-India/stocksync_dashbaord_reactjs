509dd5ec3b165e7cd0ef6ac6f0079b52aea62927
pratikshashitole29 <pratikshashitole2929@gmail.com>
2024-05-28 19:50:43 +0530

    first commit

---
+++ src/component/Footer.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+    <footer className="content-footer footer bg-footer-theme">
+  <div className="container-xxl">
+    <div className="footer-container d-flex align-items-center justify-content-between py-4 flex-md-row flex-column">
+      <div className="text-body mb-2 mb-md-0">
+        © <script>
+        document.write(new Date().getFullYear())
+
+        </script>, made with <span className="text-danger"><i className="tf-icons ri-heart-fill"></i></span> by <a href="https://themeselection.com" target="_blank" className="footer-link">ThemeSelection</a>
+      </div>
+      <div className="d-none d-lg-inline-block">
+        
+        <a href="https://themeselection.com/license/" className="footer-link me-4" target="_blank">License</a>
+        <a href="https://themeselection.com/" target="_blank" className="footer-link me-4">More Themes</a>
+        
+        <a href="https://demos.themeselection.com/materio-bootstrap-html-admin-template/documentation/" target="_blank" className="footer-link me-4">Documentation</a>
+        
+        
+        <a href="https://themeselection.com/support/" target="_blank" className="footer-link d-none d-sm-inline-block">Support</a>
+        
+      </div>
+    </div>
+  </div>
+</footer>
+
+
+
+          
+         
+    </div>
+  )
+}
+
+export default Footer
